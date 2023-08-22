ccc2d39bca663faef0a5557fd4be455ee1a57f79
nutv99 <nutv99@gmail.com>
2023-08-22 15:22:07 +0700

    Update test.php

--- api/test.php
+++ api/test.php
@@ -4,4 +4,31 @@ header('X-PHP: test');
 http_response_code($_GET['code'] ?? 200);
 
 echo 'Test output99999';
+    ob_start();
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+	
+    $token = "EfFZH8Q3NZq6BUovs0TDHSCDCC8KMHSraCLSczapf6p" ;
+	$message = 'aaaaa';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "message=" . $message);
+    $headers = array('Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer ' . $token . '',);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+
+    if (curl_error($ch)) {
+        echo 'error:' . curl_error($ch);
+    } else {
+        $res = json_decode($result, true);
+        echo "status : " . $res['status'];
+        echo "message : " . $res['message'];
+    }
+    curl_close($ch);
 
